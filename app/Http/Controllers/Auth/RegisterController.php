a0f9b5bc5f13d04b33f786fcf0c5c34e02a5930b
Farrukh Malek <jfukki@gmail.com>
2022-07-23 02:01:07 +0500

    user register

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
@@ -14,7 +16,24 @@ class RegisterController extends Controller
 
     public function userRegister(Request $req)
     {
-        return $req->input();
-        return view('auth.register');
+        $this->validate($req, [
+
+            'name' => 'required|max:255',
+            'email' => 'required|email|max:255',
+            'username' => 'required|max:255',
+            'password' => 'required|confirmed',
+        ]);
+
+        // create new user    //
+        
+        $user = new User();
+        $user ->name = $req->name;
+        $user ->username = $req->username;
+        $user ->email = $req->email;
+        $user ->password = Hash::make($req->password);
+
+        $user->save();
+        
+        return view('auth.login');
     }
 }
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -10,28 +10,33 @@
                 @csrf
                 <div class="mb-3">
                     <label for="name" class="form-label">Your Full Name</label>
-                    <input type="text" class="form-control" name="name" id="name" placeholder="Enter Your Name" >                     
+                    <input type="text" class="form-control" name="name" id="name" placeholder="Enter Your Name" value="{{old('name')}}" >  
+                    <small class="form-alert">@error('name')  {{$message}} @enderror</small>                   
                 </div>
 
                 <div class="mb-3">
                     <label for="username" class="form-label">Username</label>
-                    <input type="text" class="form-control" name="username" id="username" placeholder="Enter Your Username" >                     
+                    <input type="text" class="form-control" name="username" id="username" placeholder="Enter Your Username"  value="{{old('username')}}">                     
+                    <small class="form-alert">@error('username')  {{$message}} @enderror</small>   
                 </div>
 
                 <div class="mb-3">
                     <label for="exampleInputEmail1" class="form-label">Email address</label>
-                    <input type="email" class="form-control" name="email" id="exampleInputEmail1" placeholder="Enter Your Email" aria-describedby="emailHelp">
+                    <input type="email" class="form-control" name="email" id="exampleInputEmail1" value="{{old('email')}}" placeholder="Enter Your Email" aria-describedby="emailHelp">
                     <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                    <small class="form-alert">@error('email')  {{$message}} @enderror</small>   
                 </div>
 
 
                 <div class="mb-3">
                     <label for="password" class="form-label">Password</label>
                     <input type="password" class="form-control" id="password" name="password" placeholder="Enter Your Name">
+                    <small class="form-alert">@error('password')  {{$message}} @enderror</small>   
                 </div>
                 <div class="mb-3">
                     <label for="password_confirmation" class="form-label">Repeat Your Password</label>
                     <input type="password" class="form-control" id="password_confirmation" name="password_confirmation" placeholder="Please Repeat Your Password">
+                      
                 </div>
                 
                 <button type="submit" class="btn btn-outline-secondary register-btn">Register</button>
